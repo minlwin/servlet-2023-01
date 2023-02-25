db37887065a8cb08664ec7de0103d25d975c5bec
Min Lwin <lwin.zawmin@gmail.com>
2023-02-25 15:28:01 +0630

    Adding Test Data

--- lesson1/src/test/java/com/jdc/location/repo/test/StateRepoTest.java
+++ lesson1/src/test/java/com/jdc/location/repo/test/StateRepoTest.java
@@ -1,17 +1,13 @@
 package com.jdc.location.repo.test;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class StateRepoTest {
 
 	@ParameterizedTest
-	@CsvSource(value = {
-			"Demo Name,Demo Region,Demo Capital"
-	})
+	@CsvFileSource(resources = "/state_text.txt", delimiter = '\t')
 	void test_create(String name, String region, String capital) {
-		System.out.println("Name    : %s".formatted(name));
-		System.out.println("Region  : %s".formatted(region));
-		System.out.println("Capital : %s".formatted(capital));
+
 	}
 }
