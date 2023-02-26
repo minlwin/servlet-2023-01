73cfdc5e005a11b712fca97715bf4837b9cffd84
Min Lwin <lwin.zawmin@gmail.com>
2023-02-26 23:26:45 +0630

    Update for Homeworks

--- lesson3/src/main/java/com/jdc/location/controller/DivisionController.java
+++ lesson3/src/main/java/com/jdc/location/controller/DivisionController.java
@@ -1,5 +1,23 @@
 package com.jdc.location.controller;
 
-public class DivisionController {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {
+		"/division"
+})
+public class DivisionController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/division/list.jsp").forward(req, resp);
+	}
 
 }
--- lesson3/src/main/java/com/jdc/location/controller/TownshipController.java
+++ lesson3/src/main/java/com/jdc/location/controller/TownshipController.java
@@ -1,5 +1,23 @@
 package com.jdc.location.controller;
 
-public class TownshipController {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {
+		"/township"
+})
+public class TownshipController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/township/list.jsp").forward(req, resp);
+	}
 
 }
